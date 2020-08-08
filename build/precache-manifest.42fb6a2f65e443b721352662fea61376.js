b288f947e1e8f0c5733facfa14368fea9170bc91
sarykemmoun2 <sary2fik@gmail.com>
2020-08-09 00:59:40 +0300

    Remove from git ignore

---
+++ build/precache-manifest.42fb6a2f65e443b721352662fea61376.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6984a3b775a1358ce436bb6551b13e1f",
+    "url": "/sarykemmoun2/react-loginindex.html"
+  },
+  {
+    "revision": "46be079904a676a581e7",
+    "url": "/sarykemmoun2/react-loginstatic/js/main.ba60d21b.chunk.js"
+  },
+  {
+    "revision": "e7d5340dde137628807004374a9b7592",
+    "url": "/sarykemmoun2/react-loginstatic/js/main.ba60d21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee17ea608238a705579e",
+    "url": "/sarykemmoun2/react-loginstatic/js/runtime-main.1032432d.js"
+  }
+]);

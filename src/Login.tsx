9c3bdae2ab1f8efdc2780a6f60b507969f3e1fcb
sarykemmoun2 <sary2fik@gmail.com>
2020-08-12 12:24:02 +0300

    Try to fix error

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Login: React.FC<{text: string; onLogin: () =>{}}> = ({ text, onLogin }) => {
+const Login: React.FC<{text: string; onLogin: () =>{}; className:string}> = ({ text, onLogin, className }) => {
     return (
-        <button onClick={onLogin}>{text}</button>
+        <button className={className} onClick={onLogin}>{text}</button>
     )
 }
 export default Login;
